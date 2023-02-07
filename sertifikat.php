4b09c88f4e8e472df2fe0d89217dab1579bc6695
tuman45 <qblmahmudi@gmail.com>
2023-02-07 22:41:34 +0700

    add tempat dan tgl sertifikat

--- sertifikat.php
+++ sertifikat.php
@@ -36,16 +36,26 @@ $nama = $data['nama'];
 $awalpkl = tgl_ind($data["awalpkl"]);
 $akhirpkl = tgl_ind($data["akhirpkl"]);
 $tgl_sertifikat = $awalpkl . ' sampai dengan ' . $akhirpkl;
+$tgl_sekarang = tgl_ind(date("Y-m-d"));
 $image = 'img/sertifikat.jpg';
 
+// Set PDF
 $pdf = new FPDF();
 $pdf->AddPage('L', 'A4');
 $pdf->SetTitle('Sertifikat PRAKERIN');
+// Set Image
 $pdf->Image($image, 0, 0, 297, 210);
+// Set Name Sertifikat
 $pdf->SetFont('arial', '', 25);
 $pdf->SetY(80);
 $pdf->MultiCell(0, 10, $nama, 0, 'C', 0);
+// Set Tgl PKL
 $pdf->SetFont('times', '', 22);
 $pdf->SetY(106);
 $pdf->MultiCell(0, 10, $tgl_sertifikat, 0, 'C', 0);
-$pdf->Output('D', 'Sertifikat ' . $nama . '.pdf', false);
+// Set Tgl 
+$pdf->SetFont('times', '', 15);
+$pdf->SetXY(25, 150);
+$pdf->Cell(0, 10, "Sidoarjo, $tgl_sekarang", 0, 0);
+// Output
+$pdf->Output('I', 'Sertifikat ' . $nama . '.pdf', false);
