b1d4b096d94a5ed648a3dc158b577675127ccb75
AKIRA44AA <114198274+AKIRA44AA@users.noreply.github.com>
2023-03-26 20:20:34 +0700

    update logout modal

--- topbar.php
+++ topbar.php
@@ -175,7 +175,7 @@
                                     Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
+                                <a class="dropdown-item" onclick="logout()">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
                                 </a>
@@ -183,23 +183,22 @@
                         </li>
 
                     </ul>
-                    <!-- Logout Modal-->
-                    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">×</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                                <div class="modal-footer">
-                                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                                    <a class="btn btn-danger" href="logout.php?destroy">Logout</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <!-- Sweet Alert Logout Modal-->
+                    <script>
+                        function logout() {
+                            Swal.fire({
+                                title: 'Are you sure you want to logout?',
+                                icon: 'question',
+                                showCancelButton: true,
+                                confirmButtonText: 'Yes, logout',
+                                cancelButtonText: 'Cancel',
+                                reverseButtons: true
+                            }).then((result) => {
+                                if (result.isConfirmed) {
+                                    window.location.href = 'logout.php?destroy';
+                                }
+                            });
+                        }
+                    </script>
                 </nav>
                 <!-- End of Topbar -->
